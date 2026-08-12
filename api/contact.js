ecb3698b650d2f52043ccb11f6ff395fa3ad24a6
pavankrishtaiahveguru <pavankrishtaiah@gmail.com>
2026-08-12 10:44:14 +0530

    Fix Resend contact email configuration

--- api/contact.js
+++ api/contact.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
 
     const { data, error } = await resend.emails.send({
       from: "7Sigma Solutions <onboarding@resend.dev>",
-      to: ["sigma7tech@outlook.com"],
+      to: ["pavankrishtaiah@gmail.com"],
       replyTo: email,
       subject: `New Contact Inquiry - ${service}`,
 
